a2791cbe4ff45e974bc3e5bda865fb912bd26903
614restore <614restorellc@gmail.com>
2026-03-17 13:15:15 -0400

    Debug register MultiShotCamera plugin

--- ios/App/App/Plugins/BridgeViewController.swift
+++ ios/App/App/Plugins/BridgeViewController.swift
@@ -4,6 +4,21 @@ import Capacitor
 public class BridgeViewController: CAPBridgeViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
-        bridge?.registerPluginType(MultiShotCameraPlugin.self)
+        if bridge == nil {
+            NSLog("MultiShotCamera: bridge is nil in viewDidLoad")
+        } else {
+            NSLog("MultiShotCamera: registering in viewDidLoad")
+            bridge?.registerPluginType(MultiShotCameraPlugin.self)
+        }
+    }
+
+    override public func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if bridge == nil {
+            NSLog("MultiShotCamera: bridge is nil in viewDidAppear")
+        } else {
+            NSLog("MultiShotCamera: registering in viewDidAppear")
+            bridge?.registerPluginType(MultiShotCameraPlugin.self)
+        }
     }
 }
