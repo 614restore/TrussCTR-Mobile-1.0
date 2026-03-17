7f9b7e8605f5d9c7a5665c5bae17c052e782636c
614restore <614restorellc@gmail.com>
2026-03-17 11:56:32 -0400

    Revert AppDelegate and bridge controller

--- ios/App/App/Plugins/BridgeViewController.swift
+++
@@ -1,9 +0,0 @@
-import Capacitor
-
-@objc(BridgeViewController)
-public class BridgeViewController: CAPBridgeViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        bridge?.registerPluginInstance(MultiShotCameraPlugin())
-    }
-}
